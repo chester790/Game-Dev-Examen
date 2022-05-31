263f6c9b18e261512bd4714534ffe9d32552740d
chester790 <cbornhijm@roc-dev.com>
2022-05-31 12:09:35 +0200

    Add Game Project

---
+++ Assets/Ball.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rg;
+    public float speed;
+    private int lPunt, rPunt;
+    public TextMeshProUGUI textR, textL;
+    void Start()
+    {
+        Launch();
+    }
+
+    void Launch()
+    {
+        transform.position = Vector2.zero;
+        float x = Random.Range(-1f, 2f);
+        float y = Random.Range(-1f, 2f);
+        rg.velocity = new Vector2(x * speed, y * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Links")
+        {
+            rPunt++;
+            textR.text = rPunt.ToString();
+            Launch();
+        }
+        else if (collision.gameObject.tag == "Rechts")
+        {
+            lPunt++;
+            textL.text = lPunt.ToString();
+            Launch();
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rg;
+    public bool left;
+
+    void Update()
+    {
+        if (left)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rg.velocity = new Vector2(0f,speed);
+            } 
+                else if (Input.GetKey(KeyCode.S))
+            {
+                rg.velocity = new Vector2(0f, -speed);
+            }   
+                else
+            {
+                rg.velocity = new Vector2(0f, 0f);
+            }
+        }
+                else
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rg.velocity = new Vector2(0f, speed);
+            }
+                else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rg.velocity = new Vector2(0f, -speed);
+            }
+                else
+            {
+                rg.velocity = new Vector2(0f, 0f);
+            }
+        }
+    }
+}
